9ef66f2210f1cc2ff4cc9a52f48519fbbb8b7deb
sahar118 <sahar.t118@gmail.com>
2023-03-12 11:29:44 +0200

    add files

--- src/pages/BookAppointment/BookAppointment.js
+++ src/pages/BookAppointment/BookAppointment.js
@@ -7,6 +7,7 @@ import { showLoader } from '../../redux/loaderSlice';
 import { GetDiagnosicById } from '../../apicalls/Diagnosic'
 
 const BookAppointment = () => {
+    const [date, setDate] = useState("")
     const [diagnostic, setDiagnostic] = useState([]);
     const { id } = useParams();
     const dispatch = useDispatch();
@@ -27,6 +28,14 @@ const BookAppointment = () => {
         }
     };
 
+    const getSlotsData = () => {
+        return
+        <>
+
+        </>
+
+    }
+
     useEffect(() => {
         getData()
     }, [id])
@@ -35,11 +44,54 @@ const BookAppointment = () => {
         diagnostic && (
             <div className='bg-white p-2 my-1'>
                 <h1>
-                    {diagnostic.firstName}{diagnostic.lastName}
-                    <hr />
-                    .w-400
-                </h1>
+                    <b>
+                        {diagnostic.firstName}{diagnostic.lastName}
+                    </b>       </h1>
+                <hr />
+
+                <div className="w-400 flex flex-col gap-1 my-1">
+
+                    <div className="flex justify-between w-full book-info">
+                        <h3><b>ניסיון תעסוקתי:</b></h3>
+                        <h3> {diagnostic.experience} שנים</h3>
+                    </div>
+
+                    <div className="flex justify-between w-full book-info">
+                        <h3><b> דואר אלקטרוני:</b></h3>
+                        <h3> {diagnostic.email}</h3>
+                    </div>
+
+                    <div className="flex justify-between w-full book-info">
+                        <h3><b> טלפון</b></h3>
+                        <h3> {diagnostic.phone}</h3>
+                    </div>
+
+                    <div className="flex justify-between w-full book-info">
+                        <h3><b> כתובת</b></h3>
+                        <h3> {diagnostic.address}</h3>
+                    </div>
+
+                    <div className="flex justify-between w-full book-info">
+                        <h3><b> מחיר עבור אבחון </b></h3>
+                        <h3> {diagnostic.fee} ש"ח</h3>
+                    </div>
+                </div>
+                <hr />
+
+                {/* slot */}
+                <div className="flex flex-col gap-1 my-2">
+                    <div className="flex gap-2 w-400 items-end">
+                        <div>
+                            <span>נא לבחור תאריך  </span>
+                            <input type='date' value={date} onChange={(e) => setDate(e.target.value)} />
+                        </div>
+                        <button className='outlined-btn'>חפש </button>
+                    </div>
+                    {getSlotsData()}
+
+                </div>
             </div>
+
         )
 
     )
