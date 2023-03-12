1b220cd52b51e3adc50fe4e69e985c7a6f64f0f4
sahar118 <sahar.t118@gmail.com>
2023-03-12 02:22:43 +0200

    add files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import { async } from '@firebase/util'
+import { message } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 
+
 const Home = () => {
+    const [diagnosics, setDiagnosics] = useState([])
+    const dispatch = useDispatch()
+
+    const getData = async () => {
+        try {
+
+        } catch (error) {
+            message.error(error.message)
+            dispatch()
+
+        }
+
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
     const navigate = useNavigate()
     return (
         <div>
